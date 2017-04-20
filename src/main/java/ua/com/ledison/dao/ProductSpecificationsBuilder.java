e77e5099e4829576a1773a9a1a530fb298eb5ab2
Ivan <i1kondratiuk@gmail.com>
2017-04-20 16:13:49 +0300

    Create Specification<Product> builder

---
+++ src/main/java/ua/com/ledison/dao/ProductSpecificationsBuilder.java
@@ -0,0 +1,40 @@
+package ua.com.ledison.dao;
+
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.domain.Specifications;
+import ua.com.ledison.entity.Product;
+import ua.com.ledison.util.SearchCriteria;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class ProductSpecificationsBuilder {
+
+	private final List<SearchCriteria> params;
+
+	public ProductSpecificationsBuilder() {
+		params = new ArrayList<>();
+	}
+
+	public ProductSpecificationsBuilder with(String key, String operation, Object value) {
+		params.add(new SearchCriteria());
+		return this;
+	}
+
+	public Specification<Product> build() {
+		if (params.size() == 0) {
+			return null;
+		}
+
+		List<Specification<Product>> specs = new ArrayList<>();
+		for (SearchCriteria param : params) {
+			specs.add(new ProductSpecification(param));
+		}
+
+		Specification<Product> result = specs.get(0);
+		for (int i = 1; i < specs.size(); i++) {
+			result = Specifications.where(result).and(specs.get(i));
+		}
+		return result;
+	}
+}
