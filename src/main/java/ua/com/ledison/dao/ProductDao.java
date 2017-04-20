5c693fd503d760358dedac51e482243d71799965
Ivan <i1kondratiuk@gmail.com>
2017-04-20 16:14:24 +0300

    Polishing

--- src/main/java/ua/com/ledison/dao/ProductDao.java
+++ src/main/java/ua/com/ledison/dao/ProductDao.java
@@ -1,7 +1,8 @@
 package ua.com.ledison.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import ua.com.ledison.entity.Product;
 
-public interface ProductDao extends JpaRepository<Product, Integer> {
+public interface ProductDao extends JpaRepository<Product, Integer>, JpaSpecificationExecutor<Product> {
 }
