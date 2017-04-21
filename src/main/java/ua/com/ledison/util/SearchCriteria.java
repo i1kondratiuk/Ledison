1bd64aec49872392a23a6b483fc7c439ff3bcf74
Ivan <i1kondratiuk@gmail.com>
2017-04-21 18:15:39 +0300

    Add constructor with all class fields

--- src/main/java/ua/com/ledison/util/SearchCriteria.java
+++ src/main/java/ua/com/ledison/util/SearchCriteria.java
@@ -2,19 +2,25 @@ package ua.com.ledison.util;
 
 public class SearchCriteria {
 
-	String key;
-	String operation;
-	String value;
+	private String key;
+	private String operation;
+	private Object value;
 
 	public SearchCriteria() {
 
+	}
+
+	public SearchCriteria(final String key, final String operation, final Object value) {
+		this.key = key;
+		this.operation = operation;
+		this.value = value;
 	}
 
 	public String getKey() {
 		return key;
 	}
 
-	public void setKey(String key) {
+	public void setKey(final String key) {
 		this.key = key;
 	}
 
@@ -22,15 +28,20 @@ public class SearchCriteria {
 		return operation;
 	}
 
-	public void setOperation(String operation) {
+	public void setOperation(final String operation) {
 		this.operation = operation;
 	}
 
-	public String getValue() {
+	public Object getValue() {
 		return value;
 	}
 
-	public void setValue(String value) {
+	public void setValue(final Object value) {
 		this.value = value;
+	}
+
+	@Override
+	public String toString() {
+		return "SearchCriteria: {" + "key=" + key + ", operation=" + operation + ", value=" + value + "}";
 	}
 }
