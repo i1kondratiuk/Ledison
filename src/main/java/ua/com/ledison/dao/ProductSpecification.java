a0505c4b206767ca24a383ad547345cc3e3d83df
Ivan <i1kondratiuk@gmail.com>
2017-04-24 23:01:36 +0300

    Add dynamic selection logic

--- src/main/java/ua/com/ledison/dao/ProductSpecification.java
+++ src/main/java/ua/com/ledison/dao/ProductSpecification.java
@@ -22,16 +22,22 @@ public class ProductSpecification implements Specification<Product> {
 
 		if (criteria.getOperation().equalsIgnoreCase(">")) {
 			return builder.greaterThanOrEqualTo(
-					root.<String> get(criteria.getKey()), criteria.getValue().toString());
+					root.get(criteria.getKey()), criteria.getValue().toString());
 		}
 		else if (criteria.getOperation().equalsIgnoreCase("<")) {
 			return builder.lessThanOrEqualTo(
-					root.<String> get(criteria.getKey()), criteria.getValue().toString());
+					root.get(criteria.getKey()), criteria.getValue().toString());
 		}
 		else if (criteria.getOperation().equalsIgnoreCase(":")) {
 			if (root.get(criteria.getKey()).getJavaType() == String.class) {
-				return builder.like(
-						root.<String>get(criteria.getKey()), "%" + criteria.getValue() + "%");
+				if (criteria.getValue().toString().contains(";")) {
+					String[] values = criteria.getValue().toString().split(";");
+					for (String value : values) {
+						return builder.and(builder.equal(root.get(criteria.getKey()), value));
+					}
+				}
+				System.out.println("builder*******************" + builder);
+				return builder.like(root.get(criteria.getKey()), "%" + criteria.getValue() + "%");
 			} else {
 				return builder.equal(root.get(criteria.getKey()), criteria.getValue());
 			}
