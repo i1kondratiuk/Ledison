b5f49a1bdcbc942a06a5e977d8a61bc4d8d8375a
Ivan <i1kondratiuk@gmail.com>
2017-05-09 21:04:50 +0300

    Add search for related columns using Reflection API

--- src/main/java/ua/com/ledison/service/ProductSpecification.java
+++ src/main/java/ua/com/ledison/service/ProductSpecification.java
@@ -10,43 +10,45 @@ import java.util.List;
 
 public class ProductSpecification implements Specification<Product> {
 
-    private SearchCriteria criteria;
+	private SearchCriteria criteria;
 
-    public ProductSpecification(final SearchCriteria criteria) {
-        this.criteria = criteria;
-    }
+	public ProductSpecification(final SearchCriteria criteria) {
+		this.criteria = criteria;
+	}
 
-    @Override
-    public Predicate toPredicate(Root<Product> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
+	@Override
+	public Predicate toPredicate(Root<Product> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
 
-        if (criteria.getOperation().equalsIgnoreCase(">")) {
-            return builder.greaterThanOrEqualTo(
-                    root.get(criteria.getKey()), criteria.getValue().toString());
-        } else if (criteria.getOperation().equalsIgnoreCase("<")) {
-            return builder.lessThanOrEqualTo(
-                    root.get(criteria.getKey()), criteria.getValue().toString());
-        } else if (criteria.getOperation().equalsIgnoreCase(":")) {
-            if (criteria.getValue().toString().contains(";")) {
-                String[] values = criteria.getValue().toString().split(";");
-                List<Predicate> predicates = new ArrayList<>();
-                if (criteria.getKey().equals("productManufacturerId")){
-                    for (String value : values) {
-                        predicates.add(builder.equal(root.join("productManufacturer").get(criteria.getKey()), value));
-                    }
-                } else {
-                    for (String value : values) {
-                        predicates.add(builder.equal(root.get(criteria.getKey()), value));
-                    }
-                }
-                Predicate[] p = predicates.toArray(new Predicate[predicates.size()]);
-                return builder.or(p);
-            } else {
-                if (criteria.getKey().equals("productManufacturerId")){
-                    return builder.equal(root.join("productManufacturer").get(criteria.getKey()), criteria.getValue());
-                }
-                return builder.like(root.get(criteria.getKey()), "%" + criteria.getValue() + "%");
-            }
-        }
-        return null;
-    }
+		if (criteria.getOperation().equalsIgnoreCase(">")) {
+			return builder.greaterThanOrEqualTo(
+					root.get(criteria.getKey()), criteria.getValue().toString());
+		} else if (criteria.getOperation().equalsIgnoreCase("<")) {
+			return builder.lessThanOrEqualTo(
+					root.get(criteria.getKey()), criteria.getValue().toString());
+		} else if (criteria.getOperation().equalsIgnoreCase(":")) {
+			if (criteria.getValue().toString().contains(";")) {
+				String[] values = criteria.getValue().toString().split(";");
+				List<Predicate> predicates = new ArrayList<>();
+				if (criteria.isJoinColumn()) {
+					for (String value : values) {
+						predicates.add(builder.equal(root.join(criteria.getJoinEntity()).get(criteria.getKey()), value));
+					}
+				} else {
+					for (String value : values) {
+						predicates.add(builder.equal(root.get(criteria.getKey()), value));
+					}
+				}
+				Predicate[] p = predicates.toArray(new Predicate[predicates.size()]);
+				return builder.or(p);
+			} else {
+				if (criteria.isJoinColumn()) {
+					return builder.equal(root.join(criteria.getJoinEntity()).get(criteria.getKey()), criteria.getValue());
+				}
+				return builder.like(root.get(criteria.getKey()), "%" + criteria.getValue() + "%");
+			}
+		}
+		return null;
+	}
+
+
 }
--- src/main/java/ua/com/ledison/util/SearchCriteria.java
+++ src/main/java/ua/com/ledison/util/SearchCriteria.java
@@ -1,19 +1,56 @@
 package ua.com.ledison.util;
 
+import ua.com.ledison.entity.Product;
+
+import javax.persistence.JoinColumn;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
 public class SearchCriteria {
 
 	private String key;
 	private String operation;
 	private Object value;
+	private String joinEntity;
+
+	static Map<String, String> joinColumns;
 
 	public SearchCriteria() {
-
 	}
 
 	public SearchCriteria(final String key, final String operation, final Object value) {
 		this.key = key;
 		this.operation = operation;
 		this.value = value;
+		findJoinColumns();
+	}
+
+	private void findJoinColumns() {
+		if (joinColumns == null) {
+			joinColumns = new HashMap<>();
+			Field[] fields = Product.class.getDeclaredFields();
+			for (Field field : fields) {
+				Annotation[] annotations = field.getDeclaredAnnotations();
+				for (Annotation annotation : annotations) {
+					if (annotation instanceof JoinColumn) {
+						JoinColumn joinColumnAnnotation = (JoinColumn) annotation;
+						joinColumns.put(joinColumnAnnotation.name(), field.getName());
+					}
+				}
+			}
+		}
+	}
+
+	public boolean isJoinColumn() {
+		for (Map.Entry<String, String> entry : joinColumns.entrySet()) {
+			if (entry.getKey().equals(this.getKey())) {
+				setJoinEntity(entry.getValue());
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public String getKey() {
@@ -38,6 +75,14 @@ public class SearchCriteria {
 
 	public void setValue(final Object value) {
 		this.value = value;
+	}
+
+	public String getJoinEntity() {
+		return joinEntity;
+	}
+
+	public void setJoinEntity(String joinEntity) {
+		this.joinEntity = joinEntity;
 	}
 
 	@Override
