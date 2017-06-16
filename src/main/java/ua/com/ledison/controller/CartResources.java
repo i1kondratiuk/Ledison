8e87639149b38fecabcbff789689ca1e82602458
i1kondratiuk <i1kondratiuk@gmail.com>
2017-06-16 23:35:13 +0300

    Edit getting cart from cookies

--- src/main/java/ua/com/ledison/controller/CartResources.java
+++ src/main/java/ua/com/ledison/controller/CartResources.java
@@ -28,125 +28,134 @@ import static ua.com.ledison.util.Math.roundDoubleValue;
 @RequestMapping("/rest/cart")
 public class CartResources {
 
-	@Autowired
-	private CartService cartService;
+    @Autowired
+    private CartService cartService;
 
-	@Autowired
-	private CartItemService cartItemService;
+    @Autowired
+    private CartItemService cartItemService;
 
-	@Autowired
-	UserService userService;
+    @Autowired
+    UserService userService;
 
-	@Autowired
-	private ProductService productService;
+    @Autowired
+    private ProductService productService;
 
-	@GetMapping
-	public Cart getCart(HttpServletRequest request, HttpServletResponse response) {
-		Cookie[] cookie = request.getCookies();
-		Cart cart = new Cart(); // !!!
+    @GetMapping
+    public Cart getCart(@CookieValue(value = "cart", defaultValue = "defaultCookieValue")
+                                String cart) {
+        System.out.println(cart);
 
-		return cart;
-	}
+        return new Cart();
+    }
 
-	@GetMapping("/{cartId}")
-	public Cart getCartById(@PathVariable(value = "cartId") int cartId) {
-		return cartService.getCartById(cartId);
-	}
+    @GetMapping("/{cartId}")
+    public Cart getCartById(@PathVariable(value = "cartId") int cartId) {
+        return cartService.getCartById(cartId);
+    }
 
-	@GetMapping("/add/{productId}")
-	@Transactional
-	@ResponseStatus(value = HttpStatus.NO_CONTENT)
-	public void addItem(@PathVariable(value = "productId") int productId, Principal principal, HttpServletResponse response) {
+    @GetMapping("/add/{productId}")
+    @Transactional
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void addItem(@PathVariable(value = "productId") int productId,
+                        Principal principal, HttpServletRequest request,
+                        HttpServletResponse response) {
 
-		if (principal == null) {
-			Cart cart = new Cart();
-			Product product = productService.getProductById(productId);
-			List<CartItem> cartItems;
-			if (cart.getCartItems() == null) {
-				cartItems = new ArrayList<>();
-			} else {
-				cartItems = cart.getCartItems();
-			}
+        if (principal == null) {
+            Cart cart = new Cart();
+            Product product = productService.getProductById(productId);
+            List<CartItem> cartItems;
+            if (cart.getCartItems() == null) {
+                cartItems = new ArrayList<>();
+            } else {
+                cartItems = cart.getCartItems();
+            }
 
-			for (int i = 0; i < cartItems.size(); i++) {
-				if (product.getProductId() == cartItems.get(i).getProduct().getProductId()) {
-					CartItem cartItem = cartItems.get(i);
-					cartItem.setQuantity(cartItem.getQuantity() + 1);
-					cartItem.setTotalPrice(roundDoubleValue(product.getProductPrice() * cartItem.getQuantity(), 2));
-					Double grandTotalRounded = roundDoubleValue(cart.getGrandTotal() + cartItem.getProduct().getProductPrice(), 2);
-					cart.setGrandTotal(grandTotalRounded);
+            for (int i = 0; i < cartItems.size(); i++) {
+                if (product.getProductId() == cartItems.get(i).getProduct().getProductId()) {
+                    CartItem cartItem = cartItems.get(i);
+                    cartItem.setQuantity(cartItem.getQuantity() + 1);
+                    cartItem.setTotalPrice(roundDoubleValue(product.getProductPrice() * cartItem.getQuantity(), 2));
+                    Double grandTotalRounded = roundDoubleValue(cart.getGrandTotal() + cartItem.getProduct().getProductPrice(), 2);
+                    cart.setGrandTotal(grandTotalRounded);
 
-					return;
-				}
-			}
+                    return;
+                }
+            }
 
-			CartItem cartItem = new CartItem();
-			cartItem.setProduct(product);
-			cartItem.setQuantity(1);
-			cartItem.setTotalPrice(roundDoubleValue(product.getProductPrice() * cartItem.getQuantity(), 2));
-			cartItem.setCart(cart);
-			Double grandTotalRounded = roundDoubleValue(cart.getGrandTotal() + cartItem.getProduct().getProductPrice(), 2);
-			cart.setGrandTotal(grandTotalRounded);
-			cartItems.add(cartItem);
-			cart.setCartItems(cartItems);
+            CartItem cartItem = new CartItem();
+            cartItem.setProduct(product);
+            cartItem.setQuantity(1);
+            cartItem.setTotalPrice(roundDoubleValue(product.getProductPrice() * cartItem.getQuantity(), 2));
+            cartItem.setCart(cart);
+            Double grandTotalRounded = roundDoubleValue(cart.getGrandTotal() + cartItem.getProduct().getProductPrice(), 2);
+            cart.setGrandTotal(grandTotalRounded);
+            cartItems.add(cartItem);
+            cart.setCartItems(cartItems);
 
-			Cookie cookie = new Cookie("cart", Arrays.asList(cart.toString()).toString());
-			cookie.setMaxAge(864000);
-			response.addCookie(cookie);
-			return;
-		}
+            Cookie[] cookies = request.getCookies();
+            if (cookies != null) {
+                Arrays.stream(cookies)
+                        .forEach(c -> System.out.println(c.getName() + "=" + c.getValue()));
+            }
 
-		User user = userService.findByNameAndFetchItems(principal.getName());
+            Cookie newCookie = new Cookie("cart", Arrays.asList(cart.toString()).toString());
+            newCookie.setMaxAge(24 * 60 * 60);
+            response.addCookie(newCookie);
 
-		Cart cart = user.getCart();
-		Product product = productService.getProductById(productId);
-		List<CartItem> cartItems = cart.getCartItems();
+            return;
+        }
 
-		for (int i = 0; i < cartItems.size(); i++) {
-			if (product.getProductId() == cartItems.get(i).getProduct().getProductId()) {
-				CartItem cartItem = cartItems.get(i);
-				cartItem.setQuantity(cartItem.getQuantity() + 1);
-				cartItem.setTotalPrice(roundDoubleValue(product.getProductPrice() * cartItem.getQuantity(), 2));
-				cartItemService.addCartItem(cartItem);
-				Double grandTotalRounded = roundDoubleValue(cart.getGrandTotal() + cartItem.getProduct().getProductPrice(), 2);
-				cart.setGrandTotal(grandTotalRounded);
-				cartService.update(cart);
+        User user = userService.findByNameAndFetchItems(principal.getName());
 
-				return;
-			}
-		}
+        Cart cart = user.getCart();
+        Product product = productService.getProductById(productId);
+        List<CartItem> cartItems = cart.getCartItems();
 
-		CartItem cartItem = new CartItem();
-		cartItem.setProduct(product);
-		cartItem.setQuantity(1);
-		cartItem.setTotalPrice(roundDoubleValue(product.getProductPrice() * cartItem.getQuantity(), 2));
-		cartItem.setCart(cart);
-		cartItemService.addCartItem(cartItem);
-		Double grandTotalRounded = roundDoubleValue(cart.getGrandTotal() + cartItem.getProduct().getProductPrice(), 2);
-		cart.setGrandTotal(grandTotalRounded);
-		cart.setCartItems(cartItems);
-		cartService.update(cart);
-	}
+        for (int i = 0; i < cartItems.size(); i++) {
+            if (product.getProductId() == cartItems.get(i).getProduct().getProductId()) {
+                CartItem cartItem = cartItems.get(i);
+                cartItem.setQuantity(cartItem.getQuantity() + 1);
+                cartItem.setTotalPrice(roundDoubleValue(product.getProductPrice() * cartItem.getQuantity(), 2));
+                cartItemService.addCartItem(cartItem);
+                Double grandTotalRounded = roundDoubleValue(cart.getGrandTotal() + cartItem.getProduct().getProductPrice(), 2);
+                cart.setGrandTotal(grandTotalRounded);
+                cartService.update(cart);
 
-	@GetMapping("/remove/{cartItemId}")
-	public String removeItem(@PathVariable(value = "cartItemId") int cartItemId) {
-		CartItem cartItem = cartItemService.getCartItemById(cartItemId);
-		Cart cart = cartItem.getCart();
-		cart.setGrandTotal(roundDoubleValue(cartItem.getCart().getGrandTotal() - cartItem.getTotalPrice(), 2));
-		cartService.update(cart);
-		cartItem.getCart().getGrandTotal();
-		cartItemService.deleteCartItem(cartItem);
+                return;
+            }
+        }
 
-		return "redirect:/customer/cart/";
-	}
+        CartItem cartItem = new CartItem();
+        cartItem.setProduct(product);
+        cartItem.setQuantity(1);
+        cartItem.setTotalPrice(roundDoubleValue(product.getProductPrice() * cartItem.getQuantity(), 2));
+        cartItem.setCart(cart);
+        cartItemService.addCartItem(cartItem);
+        Double grandTotalRounded = roundDoubleValue(cart.getGrandTotal() + cartItem.getProduct().getProductPrice(), 2);
+        cart.setGrandTotal(grandTotalRounded);
+        cart.setCartItems(cartItems);
+        cartService.update(cart);
+    }
 
-	@GetMapping("/removeAll")
-	public String clearCart(Principal principal) {
-		Cart cart = userService.findByNameAndFetchItems(principal.getName()).getCart();
-		cart.setGrandTotal(0.0);
-		cartService.update(cart);
-		cartItemService.deleteAllCartItems(cart.getCartItems());
+    @GetMapping("/remove/{cartItemId}")
+    public String removeItem(@PathVariable(value = "cartItemId") int cartItemId) {
+        CartItem cartItem = cartItemService.getCartItemById(cartItemId);
+        Cart cart = cartItem.getCart();
+        cart.setGrandTotal(roundDoubleValue(cartItem.getCart().getGrandTotal() - cartItem.getTotalPrice(), 2));
+        cartService.update(cart);
+        cartItem.getCart().getGrandTotal();
+        cartItemService.deleteCartItem(cartItem);
 
-		return "redirect:/customer/cart/";
-	}
+        return "redirect:/customer/cart/";
+    }
+
+    @GetMapping("/removeAll")
+    public String clearCart(Principal principal) {
+        Cart cart = userService.findByNameAndFetchItems(principal.getName()).getCart();
+        cart.setGrandTotal(0.0);
+        cartService.update(cart);
+        cartItemService.deleteAllCartItems(cart.getCartItems());
+
+        return "redirect:/customer/cart/";
+    }
 }
